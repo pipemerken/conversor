cdef29dcc6c9ff46d0936c7ee329517d5a6c8bce
Felipe Castillo <felipe.rco@gmail.com>
2024-07-09 00:00:08 -0400

    Agrgando formato de Fechas

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,21 @@ const obtenerInformacion = async (selectValue) => {
 };  
 const crearGrafico = async (series) => {  
   const data = series.map((serie) => serie.valor);  
-  const fechas = series.map((serie) => serie.fecha);  
-  const fechas2 = fechas.reverse();
+  const fechas = series.map((serie) => serie.fecha); 
+  let fechasFormateadas = fechas.map(formatDate);
+//   const fechaFormateada = formatDate(fechas).map;
+//   console.log(fechaFormateada)
+console.log(fechas)
+
+
+
+//   const fechas2 = fechas.reverse();
   const ctx = document.getElementById("myChart");  
   console.log(ctx)  
   new Chart(ctx, {  
     type: "line",  
     data: {  
-      labels: fechas,  
+      labels: fechasFormateadas.reverse(),  
       datasets: [  
         {  
           label: "Historial de Indicador",  
@@ -24,6 +31,26 @@ const crearGrafico = async (series) => {
     },  
   });  
 };  
+
+function formatDate(dateString) {
+    // Crear un objeto Date a partir del string de la fecha
+    let date = new Date(dateString);
+
+    // Obtener el día, mes y año
+    let day = String(date.getDate()).padStart(2, '0'); // Asegurar que el día tenga 2 dígitos
+    let month = String(date.getMonth() + 1).padStart(2, '0'); // Los meses en JavaScript son base 0, por eso se suma 1
+    let year = date.getFullYear();
+
+    // Formatear la fecha como "dd-mm-yyyy"
+    return `${day}/${month}/${year}`;
+}
+
+
+// let fecha = "2024-05-01T04:00:00.000Z";
+// let fechaFormateada = formatDate(fecha);
+// console.log(fechaFormateada); // "08-07-2024"
+
+
 
 btnCalcular.addEventListener("click", async () => {  
   let grafico = Chart.getChart("myChart")  
@@ -38,3 +65,5 @@ btnCalcular.addEventListener("click", async () => {
   crearGrafico(respuesta.serie);  
   document.querySelector("#resultado").innerHTML =`Resuldato: $${cambio}` 
 });
+
+
